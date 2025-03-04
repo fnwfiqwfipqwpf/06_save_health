b98ec5ca8af2cdc513c5b6b5bb6e6a4bc85958aa
vitalii <24b_svv@liceum.ztu.edu.ua>
2025-03-04 22:14:45 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -61,6 +61,7 @@ console.log('btnbuyhearts clicked!');
 document.getElementById('changeBackgroundBtn').addEventListener('click', function() {
 
   document.body.style.backgroundImage = "url('../img/background2.png')";
+  console.log("Changing background to:", document.body.style.backgroundImage);
 });
 
 
